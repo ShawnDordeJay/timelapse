3cb6d405835784f0da8c07c2559ef7b8c43936fa
ShawnDordeJay <igor.jerinic@hotmail.com>
2020-07-26 22:32:59 +0200

    verschönerung des codes

--- convert_to_video.py
+++ convert_to_video.py
@@ -6,9 +6,10 @@ from shutil import copyfile
 import shutil
 import sys
 
-WAIT_TIME = 3600
+WAIT_TIME = 3600 #3600 seconds = 1 hour
 path = '/home/pi/pictures'
 
+#start every full hour
 sleep(60*(60-DateTime.now().minute))
 
 while 1 :
@@ -18,7 +19,8 @@ while 1 :
 
 	now = DateTime.now()
 	now_string = DateTime.now().strftime('%d_%m_%Y_%H_%M')
-	#create new folder and move pictures to new folder
+
+	#create new folder
 	newfolder = path+'/'+now_string
 	try:
 		os.mkdir(newfolder)
@@ -26,23 +28,31 @@ while 1 :
                 print("Creation of the directory %s failed" % newfolder)
         else:
                 print("Successfully created the directory %s " % newfolder)
+
+	#move files to new folder
+        sys.stdout.write('start converting\n')
+        sys.stdout.flush()
 	for f in files:
                 shutil.move(f, newfolder)
 
+	#converting to .mp4
 	sys.stdout.write('start converting\n')
         sys.stdout.flush()
 
 	os.system('ffmpeg -y -r 24 -pattern_type glob -i \'' + newfolder + '/img*.jpg\' -s hd1080 -vcodec libx264 timelapse.mp4')
 
+	#rename mp4
 	sys.stdout.write('rename\n')
         sys.stdout.flush()
 
 	os.rename('timelapse.mp4', now_string+'.mp4')
 
+	#move file to created folder
         sys.stdout.write('move file\n')
         sys.stdout.flush()
 	shutil.move(now_string+'.mp4', newfolder)
 
+	#remove pictures from folder
         sys.stdout.write('remove files\n')
         sys.stdout.flush()
 
@@ -50,8 +60,6 @@ while 1 :
 	for f in files :
 		os.remove(f)
 
-	#now = DateTime.now()
-        #hour = now.strftime('%H')
 
 	sys.stdout.write('sleeping\n')
 	sys.stdout.flush()
--- taking-pictures.py
+++ taking-pictures.py
@@ -1,18 +1,11 @@
 from time import sleep
 import time
-from datetime import date as Date
 from datetime import datetime as DateTime
-from datetime import timedelta as TimeDelta
 import picamera
-import glob
 import os
-import shutil
 import sys
 
 WAIT_TIME = 8
-#today = Date.today()
-now = DateTime.now()
-hour = now.strftime('%H')
 path = '/home/pi/pictures'
 
 with picamera.PiCamera() as camera:
@@ -24,40 +17,4 @@ with picamera.PiCamera() as camera:
 		sys.stdout.flush()
 		sleep(WAIT_TIME)
 		camera.annotate_text = DateTime.now().strftime('%d-%m-%Y %H:%M')
-		#if new day, generate video
-		#if today < Date.today():
-		"""
-		if now.hour < DateTime.now().hour :
-			#get files and sort by creation date
-			files = glob.glob(path+'/*.jpg')
-			files.sort(key=os.path.getmtime)
-
-			#os.system("ffmpeg -r 25 -pattern_type glob -i '/home/pi/pictures/img*.jpg' -s hd1080 -vcodec libx264 timelapse.mp4")
-			#os.rename('timelapse.mp4', time.strftime('%d%m%Y')+'.mp4')
-			#delete all pictures
-			#for file in files:
-			#	os.remove(file)
-			#set new today
-
-			sys.stdout.write('trying create new folder\n')
-                	sys.stdout.flush()
-			newfolder = path+'/'+hour
-			try:
-                                os.mkdir(newfolder)
-                        except OSError:
-                                print("Creation of the directory %s failed" % newfolder)
-                        else:
-                                print("Successfully created the directory %s " % newfolder)
-                        sys.stdout.write('move files to folder\n')
-                        sys.stdout.flush()
-
-			for f in files:
-				shutil.move(f, newfolder)
-
-			now = DateTime.now()
-			hour = now.strftime('%H')
-                        sys.stdout.write('new hour = ' + hour+'\n')
-                        sys.stdout.flush()
-		"""
-
 
